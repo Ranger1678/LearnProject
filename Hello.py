82925d1cfec4932b859ed4ea4a6fcd115190515a
Ranger <13809070778@163.com>
2020-07-21 00:08:45 +0800

    the begin of git usage

---
+++ Hello.py
@@ -0,0 +1,41 @@
+# virables test
+# message="Hello"
+# names="Eric"
+# age="16"
+# print(message)
+# print(names)
+# print(age)
+
+# string test
+# print(message+" "+names+"\ni am\t"+age+"\tyears old")
+# name=" david "
+# print(name.title())
+# name=name.rstrip()
+# print(name)
+# name=name.lstrip()
+# print(name.rstrip())
+# name=name.lower()
+# print(name)
+# name=name.upper()
+# print(name)
+
+# compute test
+# sum=2+3
+# print(sum)
+# plus=2**3
+# print(plus)
+# minus=3-2
+# print(minus)
+# a=0.1+0.1
+# print(a)
+# print("number is : "+str(a))
+
+# data change
+a = 1
+b = 2
+print(a)
+print(b)
+# 以逗号实现交换值的功能
+a, b = b, a + b
+print(a)
+print(b)
